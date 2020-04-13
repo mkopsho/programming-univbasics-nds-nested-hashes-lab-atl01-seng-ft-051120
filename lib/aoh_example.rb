e48a0bb0423647812d9eb2bc13ba82e85d062c12
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-13 21:52:58 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,9 +13,9 @@ def assembled_aoh
 end
 
 def literal_aoh
-  # Using Array literal syntax only, build a nested array that uses the data in
+  # Using Array literal syntax only, build a nested array that uses the data
   # held in the constants
-  
+  [assembled_aoh]
 end
 
 def aoh_lookup(aoh, row, key)
